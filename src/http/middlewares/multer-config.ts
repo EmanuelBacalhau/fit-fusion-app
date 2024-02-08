1da930f4edfe896a1cc4487b6dcc8f5e7b1ec388
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-02-08 12:40:26 -0300

    chore: add setting multer

---
+++ src/http/middlewares/multer-config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { randomBytes } from 'crypto'
+import multer, { diskStorage } from 'multer'
+
+const storage = (path: string) => {
+  return diskStorage({
+    destination: resolve(__dirname, '../../../uploads', path),
+    filename: (request, file, cb) => {
+      const hash = randomBytes(16).toString('hex')
+
+      const filename = `${hash}-${file.originalname}`
+
+      return cb(null, filename)
+    },
+  })
+}
+
+export const multerConfig = {
+  user: multer({ storage: storage('avatars') }),
+}
