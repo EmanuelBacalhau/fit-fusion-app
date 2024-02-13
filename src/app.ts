df707fafc2a97bd8851904b73c6be65ee6f053cf
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-02-13 15:59:36 -0300

    feat: add static routes

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,20 @@ import { dayRoutes } from './controllers/days/routes'
 import { typesExerciseRoutes } from './controllers/types-exercise/routes'
 import { exerciseRoutes } from './controllers/exercises/routes'
 import { userHasExerciseRoutes } from './controllers/user-has-exercises/routes'
+import { resolve } from 'path'
 
 export const app = express()
 
 app.use(express.json())
+
+app.use(
+  '/uploads/avatars',
+  express.static(resolve(__dirname, '../uploads/avatars')),
+)
+app.use(
+  '/uploads/exercises',
+  express.static(resolve(__dirname, '../uploads/exercises')),
+)
 
 app.use(dayRoutes)
 app.use(userRoutes)
